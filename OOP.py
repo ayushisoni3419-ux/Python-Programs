a4e008dd4fc7a1afccfd10205fd9263b19daf06f
Ayushi Soni <ayushisoni3419@gmail.com>
2026-07-20 16:35:01 +0530

    Meaningful message

---
+++ OOP.py
@@ -0,0 +1,9 @@
+class students:
+   def __init__(self,name,marks,rollno):
+    self.name=name
+    self.marks=marks
+    self.rollno=rollno
+s1=students("Ayushi",90,1)
+print(s1.name)
+print(s1.marks)
+print(s1.rollno)
